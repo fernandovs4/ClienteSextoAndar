4d55d9041b73442ff3104b44936f6afb1a8d3912
Juliakp1 <juliakp710@gmail.com>
2023-10-25 15:37:03 -0300

    verify route coded,  now to implement

--- src/main/java/com/client/cliente/ClienteService.java
+++ src/main/java/com/client/cliente/ClienteService.java
@@ -1,10 +1,15 @@
 package com.client.cliente;
 
+import com.client.cliente.dto.ReturnUserDTO;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+
 
 @Service
 public class ClienteService {
@@ -38,5 +43,15 @@ public class ClienteService {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    // verifies the token, use this on every route
+    public ReturnUserDTO verifyUser(String token) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ReturnUserDTO> response = restTemplate.getForEntity("http://54.71.150.144:8082/login/" + token, ReturnUserDTO.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody();
+        }
+        return null;
     }
 }
